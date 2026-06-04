52985835b35f5a913cd8f189cf4ad18fdfd02328
ZhaoTianyue0618 <3584327632@qq.com>
2026-06-05 01:16:58 +0800

    Delete reconstruction directory

--- reconstruction/lrm_infer.py
+++
@@ -1,9 +0,0 @@
-import sys
-from triposr import TripoSR
-import os
-img_path = sys.argv[1]
-model = TripoSR()
-out_glb = "./static/"+str(os.urandom(4).hex())+".glb"
-mesh = model(img_path)
-mesh.export(out_glb)
-print(out_glb)
